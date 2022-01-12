5bf72a274d5e6fb69afa571c6634fcb1c98602f3
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2022-01-12 02:39:01 -0500

    deleted duplicate file and also made sure the agent method was calling the proper end point to paymentController

--- src/features/checkout/StripeInput.tsx
+++ src/features/checkout/StripeInput.tsx
@@ -8,15 +8,15 @@ interface Props extends InputBaseComponentProps {
 export const StripeInput = forwardRef(function StripeInput({
   component: Component, ...props
 }: Props, ref: Ref<unknown>) {
-  const elementRef = useRef<any>();
-  useImperativeHandle(ref, () => ({
-    focus: () => elementRef.current.focus
-  }));
-  return (
-    <Component
-      onReady={(element: any) => elementRef.current = element}
-      {...props}
-    />
-    
-  )
+    const elementRef = useRef<any>();
+    useImperativeHandle(ref, () => ({
+      focus: () => elementRef.current.focus
+    }));
+    return (
+      <Component
+        onReady={(element: any) => elementRef.current = element}
+        {...props}
+      />
+      
+    )
 });
